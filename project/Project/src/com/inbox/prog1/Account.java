407b960aea13dcf6e4b556f202e07e28031a8f7a
Akshata M Palankar <116936466+Akshata192000@users.noreply.github.com>
2022-11-28 23:22:32 +0530

    Add files via upload

---
+++ project/Project/src/com/inbox/prog1/Account.java
@@ -0,0 +1,47 @@
+package com.inbox.prog1;
+
+import java.util.Scanner;
+
+class Account
+{
+	int AmtD;
+	int AmtW;
+	void Deposite()
+	{
+		//int AmtD;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Amount to Deposite");
+		AmtD=sc.nextInt();
+	}
+	void Withdraw()
+	{
+		//int AmtW;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Amount to Withdraw");
+		AmtW=sc.nextInt();
+	}
+	void Display()throws NoSufficientFundException
+	{
+	    int blnc = AmtD - AmtW;
+		if(AmtW>AmtD)
+		{
+			throw new NoSufficientFundException("InSuficient balance   " + AmtD +" Available balance");
+			//throw  NoSufficientFundException("Available Balance: "+AmtD);
+		}
+		else
+		{
+		System.out.println("Amount withdraw sucessfully Rupes " + AmtW);
+		System.out.println("Balance in Your Accunt: " + blnc);
+		}
+	}
+}
+public class Account{
+
+	public static void main(String [] args)throws NoSufficientFundException
+			{
+			Account ac= new Account();
+			ac.Deposite();
+			ac.Withdraw();
+			ac.Display();
+			}
+}
